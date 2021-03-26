81a8f27f4813f0e6de89d5d4b52270ccdad69e16
ItzAash <aashishneupane125@gmail.com>
2021-03-26 23:36:07 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,98 @@
+const data = {
+  request_hash: "request:search:79187eaf5acda44baa54b5ec4df7de06c8d65f72",
+  request_cached: false,
+  request_cache_expiry: 432000,
+  results: [
+    {
+      mal_id: 16498,
+      url: "https://myanimelist.net/anime/16498/Shingeki_no_Kyojin",
+      image_url:
+        "https://cdn.myanimelist.net/images/anime/10/47347.jpg?s=29949c6e892df123f0b0563e836d3d98",
+      title: "Shingeki no Kyojin",
+      airing: false,
+      synopsis:
+        "Centuries ago, mankind was slaughtered to near extinction by monstrous humanoid creatures called titans, forcing humans to hide in fear behind enormous concentric walls. What makes these giants truly...",
+      type: "TV",
+      episodes: 25,
+      score: 8.48,
+      start_date: "2013-04-07T00:00:00+00:00",
+      end_date: "2013-09-29T00:00:00+00:00",
+      members: 2514320,
+      rated: "R",
+    },
+    {
+      mal_id: 35760,
+      url: "https://myanimelist.net/anime/35760/Shingeki_no_Kyojin_Season_3",
+      image_url:
+        "https://cdn.myanimelist.net/images/anime/1173/92110.jpg?s=410d006fea0608544e9861a6f261c692",
+      title: "Shingeki no Kyojin Season 3",
+      airing: false,
+      synopsis:
+        'Still threatened by the "Titans" that rob them of their freedom, mankind remains caged inside the two remaining walls. Efforts to eradicate these monsters continue; however, threats arise not only fro...',
+      type: "TV",
+      episodes: 12,
+      score: 8.6,
+      start_date: "2018-07-23T00:00:00+00:00",
+      end_date: "2018-10-15T00:00:00+00:00",
+      members: 1217052,
+      rated: "R",
+    },
+    {
+      mal_id: 38524,
+      url:
+        "https://myanimelist.net/anime/38524/Shingeki_no_Kyojin_Season_3_Part_2",
+      image_url:
+        "https://cdn.myanimelist.net/images/anime/1517/100633.jpg?s=4540a01b5883647ade494cd28392f100",
+      title: "Shingeki no Kyojin Season 3 Part 2",
+      airing: false,
+      synopsis:
+        'Seeking to restore humanity\'s diminishing hope, the Survey Corps embark on a mission to retake Wall Maria, where the battle against the merciless "Titans" takes the stage once again. Returning to the...',
+      type: "TV",
+      episodes: 10,
+      score: 9.11,
+      start_date: "2019-04-29T00:00:00+00:00",
+      end_date: "2019-07-01T00:00:00+00:00",
+      members: 1078839,
+      rated: "R",
+    },
+    {
+      mal_id: 19285,
+      url:
+        "https://myanimelist.net/anime/19285/Shingeki_no_Kyojin__Ano_Hi_Kara",
+      image_url:
+        "https://cdn.myanimelist.net/images/anime/3/51715.jpg?s=c054db6a77e6ef79b01821333be32345",
+      title: "Shingeki no Kyojin: Ano Hi Kara",
+      airing: false,
+      synopsis:
+        "Recap of the events in Shingeki no Kyojin that happened prior to episode 14.",
+      type: "Special",
+      episodes: 1,
+      score: 7.14,
+      start_date: "2013-07-07T00:00:00+00:00",
+      end_date: "2013-07-07T00:00:00+00:00",
+      members: 98696,
+      rated: "R",
+    },
+    {
+      mal_id: 19391,
+      url:
+        "https://myanimelist.net/anime/19391/Shingeki_no_Kyojin_Picture_Drama",
+      image_url:
+        "https://cdn.myanimelist.net/images/anime/8/53477.jpg?s=f2398b1638ffc88556dca4dfca52fbf9",
+      title: "Shingeki no Kyojin Picture Drama",
+      airing: false,
+      synopsis:
+        "Shingeki no Kyojin Picture Drama is a series of Flash animated gag shorts included with the Blu-ray Disc/DVD releases, featuring the characters in chibi form. Consisting of nine episodes, this series...",
+      type: "Special",
+      episodes: 9,
+      score: 6.63,
+      start_date: "2013-07-17T00:00:00+00:00",
+      end_date: "2014-03-19T00:00:00+00:00",
+      members: 43780,
+      rated: "PG-13",
+    },
+  ],
+  last_page: 20,
+};
+
+export default data;
